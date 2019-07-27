d72b37216dd6db6737bcfb13dda0bf1a52a9e891
michael-leroy <michael-leroy@users.noreply.github.com>
2019-07-27 16:05:26 -0700

    Added new constructor for when a new map object is created, but other data has not been found yet.
    Now uses regex to ensure only proper lumps are added to mapdata.
    
    Added field for map name. We are also storing the lump in mapdata.

--- src/WadMap.java
+++ src/WadMap.java
@@ -1,20 +1,67 @@
 
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class WadMap {
 
-    private static final HashSet<String> validMapLumps = new HashSet<>(Arrays.asList(
-            "THINGS", "LINEDEFS", "SIDEDEFS", "VERTEXES", "SEGS", "SSECTORS", "NODES",
+    private String mapName = null;
+
+    private static final Pattern mapRegex = Pattern.compile("^E[1-4]M[0-9]$|^MAP\\d\\d$");
+
+    private static final HashSet<String> validMapLumps = new HashSet<>(Arrays.asList("THINGS",
+            "LINEDEFS", "SIDEDEFS", "VERTEXES", "SEGS", "SSECTORS", "NODES",
             "SECTORS", "REJECT", "BLOCKMAP", "BEHAVIOR", "SCRIPTS"));
 
     public HashMap<String, WadLump> mapData = new HashMap<>();
 
+    public WadMap(WadLump wadLump) {
+
+        Matcher mapMatch = mapRegex.matcher(wadLump.getFileName());
+
+        if (mapMatch.find()) {
+            System.out.println("Adding map name to mapData...");
+            this.mapData.put(wadLump.getFileName(), wadLump);
+            setMapName(wadLump.getFileName());
+
+        }
+    }
+
     public WadMap(List<WadLump> wadLumps) {
-        for (WadLump mapLump : wadLumps ) {
-            if (validMapLumps.contains(mapLump.getFileName())) {
-                mapData.put(mapLump.getFileName(), mapLump);
+        for (WadLump wadLump : wadLumps ) {
+
+            Matcher mapMatch = mapRegex.matcher(wadLump.getFileName());
+
+            if (mapMatch.find()) {
+                System.out.println("Adding map name to mapData...");
+                this.mapData.put(wadLump.getFileName(), wadLump);
+                setMapName(wadLump.getFileName());
+
+            }
+
+            if (validMapLumps.contains(wadLump.getFileName())) {
+                this.mapData.put(wadLump.getFileName(), wadLump);
             }
         }
+    }
+
+    public static HashSet<String> getValidMapLumps() {
+        return validMapLumps;
+    }
+
+    public static boolean isValidMapLump(String stringToCheck) {
+        if (validMapLumps.contains(stringToCheck)) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getMapName() {
+        return mapName;
+    }
+
+    public void setMapName(String mapName) {
+        this.mapName = mapName;
     }
 }
