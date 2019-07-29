0dc8d7d5b6efdab8f85e0cb9f7515a1a8b166147
michael-leroy <michael-leroy@users.noreply.github.com>
2019-07-29 00:37:27 -0700

    Initial check in. Basic class to store DOOM/WAS vertexes.

---
+++ src/DVertex.java
@@ -0,0 +1,26 @@
+
+public class DVertex {
+
+    private int y;
+    private int x;
+
+    public int getX() {
+        return this.x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+
+
+    public int getY() {
+        return this.y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+
+}
